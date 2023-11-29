e656869bb89739a6f7cef4fd6057ed6484e91de8
Felipemspc <mspcfelipe@gmail.com>
2023-11-29 12:17:50 -0300

    condicao simples

---
+++ Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel =parseFloat ( 61 )
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+
+if(vel > 60){ //Condição Simples (apenas um if)
+    console.log('Você ultrapassou o limite permitido! Você foi Multado!')
+
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ Aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'EUA'
+console.log(`Vivendo em ${país}.`)
+if(país != 'Brasil'){ //condição composta if e else.
+    console.log(`Você é estrangeiro!`)
+    
+}else{ //condição composta if e else.
+    console.log('Você é Brasileiro!')
+}
