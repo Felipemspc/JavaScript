19b5c627ca04873d4914820712e828ced96cec93
Felipemspc <mspcfelipe@gmail.com>
2023-12-05 13:27:08 -0300

    ex16

--- Aula14Ex/ex016/script.js
+++ Aula14Ex/ex016/script.js
@@ -1,33 +1,31 @@
 function contar() {
-    var inicio = document.getElementById('inicio').value;
-    var fim = document.getElementById('fim').value;
-    var passo = document.getElementById('passo').value;
-    var c = 1;
-    var resultado = document.getElementById('resultado');
-    var contando = document.getElementsByTagName('p')[4]
-    // Convertendo os inputs em números...
-    inicio = parseInt(inicio);
-    fim = parseInt(fim);
-    passo = parseInt(passo);
+  var inicio = document.getElementById("inicio").value;
+  var fim = document.getElementById("fim").value;
+  var passo = document.getElementById("passo").value;
+  var resultado = document.getElementById("resultado");
 
-    // validando os numeros adcionados nos inputs....
-    if (isNaN(inicio) || isNaN(fim) || isNaN(passo)) {
-        resultado.innerHTML = "Por favor, insira números válidos.";
-        return;
-    }
+  // Convertendo os inputs em números...
+  inicio = parseInt(inicio);
+  fim = parseInt(fim);
+  passo = parseInt(passo);
 
-    // Resetando o campo de resultado antes de começar... 
-    resultado.innerHTML = "Contando: <br>";
-    
-    // checando se passo é negativo ou 0
-    if (passo <= 0) {
-        resultado.innerHTML = "O passo deve ser um número positivo e diferente de zero.";
-        return;
-    }
-    
-    for (c = inicio; c <= fim; c += passo) {
-        
-        resultado.innerHTML += `${c} 👉 `;
-    }
-    resultado.innerHTML += "🏁";
-} 
+  // validando os numeros adcionados aos inputs....
+  if (isNaN(inicio) || isNaN(fim) || isNaN(passo)) {
+    alert ("Por favor, insira números válidos.")
+    return;
+  }
+
+  //Resetando campo resultado antes de iniciar...
+  resultado.innerHTML = "";
+
+  // checando se passo é negativo ou 0
+  if (passo <= 0) {
+    alert ("O passo deve ser um número positivo e diferente de zero.")
+    return;
+  }
+  resultado.innerHTML = "Contando: <br>";
+  for (c = inicio; c <= fim; c += passo) {
+    resultado.innerHTML += `${c} 👉 `;
+  }
+  resultado.innerHTML += "🏁";
+}
