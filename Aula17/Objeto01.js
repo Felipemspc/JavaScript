e85b5bc0fcbb8d9572ed08a01dff3b830125e84f
Felipemspc <mspcfelipe@gmail.com>
2023-12-18 17:41:22 -0300

    Orientação a objeto

---
+++ Aula17/Objeto01.js
@@ -0,0 +1,18 @@
+let amigo = {nome: 'José' ,
+ sexo: 'M', 
+ peso: 85.4,
+engordar(p = 0){
+    console.log('Engordou')
+    this.peso += p
+    
+}}
+
+amigo.engordar(5)
+
+/*let peso = amigo.peso
+if(amigo.peso > 100){
+    console.log(`O seu peso é de ${peso}, Faça uma dieta urgente!`)
+}else{
+}
+*/
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg.`)
